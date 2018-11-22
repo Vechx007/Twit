06e584eab96dffaabf2749a2846a0e42f1a4abf2
Vechx007 <44900134+Vechx007@users.noreply.github.com>
2018-11-22 14:17:42 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Streaming Code By : OBX King Dom`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Code By :! vM , itzZa1D.#8866 `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`test`,"http://twitch.tv/ImD3s_x")
+client.user.setStatus("online")
+});
+
+
+
+ client.login("TOKEN");
